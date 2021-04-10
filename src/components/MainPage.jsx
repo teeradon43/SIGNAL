0230147a8ddf10cf52958e53288e4315811e5417
Napat Srinamon <74853232+PuiFertilizer@users.noreply.github.com>
2021-04-11 01:04:23 +0700

    Update MainPage.jsx
    base main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,59 @@
+import React from 'react';
+import firestore, {auth, googleProvider} from '../database/firebase';
+import ReactDOM from 'react-dom'
+
 const MainPage = () => {
-    return ( 
-        <div>
-            Main Page, Yay!
+    /*const signOutHandler = () =>{
+        auth.signOut().then(()=>{
+            console.log("Logout OK")
+        })
+        .catch((err)=>{
+            console.log("Logout Error: "+err);
+        })
+    }*/
+    const createEventHandler = () =>{
+        //Redirect to createEvent
+    }
+    //TODO:fetch data from firebase and tranform eventID to eventDetail
+    //where should detail fetch
+    class Event extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state = {ID: this.props.ID};
+        }
+        render(){
+            return(
+                <div className="jumbotron">
+                    {this.state.ID}
+                </div>
+            );
+        }
+    }
+    class EventsListDisplay extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state = {EventID : [1,2,3]}//should fetch id before
+        }
+        render(){
+            return (
+                <div>
+                    {this.state.EventID.map(item=>(
+                        <Event ID = {item}/>
+                    ))}
+                </div>
+            );
+        }
+    }
+    return (  
+        <div class="container-fluid">
+            <div>
+            <button onClick={createEventHandler}>Create Event</button>
+            </div>
+            <div>
+                <EventsListDisplay/>
+            </div>
         </div>
-     );
+    );
 }
  
 export default MainPage;
