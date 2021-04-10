977af47fb4e04a612cd180a824b4a294a3b52002
Napat Srinamon <74853232+PuiFertilizer@users.noreply.github.com>
2021-04-11 01:28:10 +0700

    little more change

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import firestore, {auth, googleProvider} from '../database/firebase';
-import ReactDOM from 'react-dom'
 
 const MainPage = () => {
     /*const signOutHandler = () =>{
@@ -12,7 +11,7 @@ const MainPage = () => {
         })
     }*/
     const createEventHandler = () =>{
-        //Redirect to createEvent
+        //TODO:Redirect to createEvent
     }
     //TODO:fetch data from firebase and tranform eventID to eventDetail
     //where should detail fetch
@@ -21,10 +20,29 @@ const MainPage = () => {
             super(props);
             this.state = {ID: this.props.ID};
         }
+        fetchData(){
+
+        }
+        //TODO:add onclick redirect
         render(){
             return(
                 <div className="jumbotron">
-                    {this.state.ID}
+                    <div className = "row">
+                        <div className = "col-2">
+                            img here
+                        </div>
+                        <div className = "col-4">
+                            <div>
+                                2222
+                            </div>
+                            <div>
+                                name
+                            </div>
+                            <div>
+                                des
+                            </div>
+                        </div>
+                    </div>
                 </div>
             );
         }
@@ -32,7 +50,7 @@ const MainPage = () => {
     class EventsListDisplay extends React.Component{
         constructor(props) {
             super(props);
-            this.state = {EventID : [1,2,3]}//should fetch id before
+            this.state = {EventID : [1,2,3,4,5]}//should fetch id before
         }
         render(){
             return (
@@ -47,7 +65,7 @@ const MainPage = () => {
     return (  
         <div class="container-fluid">
             <div>
-            <button onClick={createEventHandler}>Create Event</button>
+                <button onClick={createEventHandler}>Create Event</button>
             </div>
             <div>
                 <EventsListDisplay/>
