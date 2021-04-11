a45de31e2f00ef9a7e11d5ef0439331dbeb935f2
teeradon43 <teeradonch43@gmail.com>
2021-04-12 00:28:33 +0700

    +EventDetails, Fetch& Route Event from Firestore,

---
+++ src/components/pages/EventDetail.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import firestore from "../../database/firebase";
+
+class EventDetails extends Component {
+  state = {
+    posts: {},
+  };
+  componentDidMount() {
+    const {
+      match: { params },
+    } = this.props;
+    // get firestore collection = events , document = eventId , setState of posts from doc
+    firestore
+      .collection("events")
+      .doc(params.eventId)
+      .get()
+      .then((snapshot) => {
+        this.setState({
+          posts: snapshot.data(),
+        });
+        console.log(this.state);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="event-detail">
+        <h1>{posts.eventName}</h1>
+        <p>{posts.description}</p>
+        <p>Number of Attendee : {posts.attendeeNumber}</p>
+        <p>Total of : {posts.maxAttendee}</p>
+        <p>Cost : {posts.cost}</p>
+        <p>Event Date : {posts.eventDate}</p>
+        <p>Since : {posts.dateCreated}</p>
+        <p>{posts.image}</p>
+      </div>
+    );
+  }
+}
+
+export default EventDetails;
