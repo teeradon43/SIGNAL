64720752f03b9184c7d9a3552034a5999abae2fd
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2021-04-18 15:26:12 +0700

    User confirm toggle ban+delete

--- src/components/Admin/ConfirmDeleteUser.jsx
+++ src/components/Admin/ConfirmDeleteUser.jsx
@@ -1,8 +1,31 @@
+import firestore from '../../database/firebase';
+
+import {useParams, Link} from 'react-router-dom';
+
 const ConfirmDeleteUser = () => {
-    console.log('del user')
-    return ( <div>
-        <h1>Delete User?</h1>
-    </div> );
+    const usersRef = firestore.collection('users');
+    const {userID} = useParams();
+    const deleteHandler = (id) =>{
+        usersRef.doc(id).delete().then(()=>{
+            console.log(`Deletion on user of id:${id} completed`);
+            window.location.href='/Admin/manage-user';
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+
+    return( 
+        <div className="container">
+            <h1>Delete User with ID: {userID} </h1>
+            <button className="btn btn-secondary m-2" onClick={()=>deleteHandler(userID)}>
+                Delete
+            </button>
+            <Link className="btn btn-primary m-2" to="/Admin/manage-user">
+                Cancel
+            </Link>
+        </div>
+        );
 }
  
 export default ConfirmDeleteUser;
--- src/components/Admin/ConfirmToggleBan.jsx
+++ src/components/Admin/ConfirmToggleBan.jsx
@@ -1,7 +1,40 @@
+import firestore from '../../database/firebase';
+
+import {useParams, Link} from 'react-router-dom';
+
 const ConfirmToggleBan = () => {
+    const usersRef = firestore.collection('users');
+    const {userID} = useParams();
+    const editHandler = (userID)=>{
+        usersRef.doc(userID).get().then((doc)=>{
+            if(doc.exists){
+                const user={
+                    userID: doc.data().userID,
+                    displayName: doc.data().displayName,
+                    isBanned: doc.data().isBanned,
+                };
+                usersRef.doc(user.userID).update({
+                    isBanned: !user.isBanned
+                }).then(()=>{
+                    console.log(`Toggle ban on uid:${user.userID} is completed`);
+                    window.location.href='/Admin/manage-user';
+                })
+                .catch((err)=>{
+                    console.log(err);
+                })
+            }
+        }).catch((err)=>console.log("No user: "+err)); 
+    }
+
     return ( 
-        <div>
-            Toggle Ban?
+       <div className="container">
+            <h1>Toggle Ban for User with ID: {userID}?</h1>
+            <button className="btn btn-secondary m-2" onClick={()=>editHandler(userID)}>
+                Toggle
+            </button>
+            <Link className="btn btn-primary m-2" to="/Admin/manage-user">
+                Cancel
+            </Link>
         </div>
      );
 }
