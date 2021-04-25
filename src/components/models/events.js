acd3f24c9660d5e8bc2700f40b994d1c85a5f817
teeradon43 <teeradonch43@gmail.com>
2021-04-26 02:18:03 +0700

    Add data to attendee list / eventHistory

--- src/components/models/events.js
+++ src/components/models/events.js
@@ -1,4 +1,5 @@
 import firestore from "../../database/firebase";
+import firebase from "firebase/app";
 import { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 
@@ -28,54 +29,41 @@ export const CreateEvent = (params) => {
     .add({
       event,
     })
-    .then(() => {
+    .then(function (docRef) {
+      // console.log("docRef =", docRef.id);
+      // Host joined their event
+      AddEventToUser(docRef.id, params.uid);
+      AddUserToEvent(docRef.id, params.uid);
       alert("You have created a new event!");
-      //TODO: Redirect to mainpage or eventpage
     })
     .catch((e) => {
       alert("Error", e);
     });
-
-  // }
 };
 
 //------------- Update Event ----------- //
 export const UpdateEvent = (eventId, params) => {
-  const baseEvent = GetEvent(eventId);
-  const event = {
-    uid: params.uid,
-    title: params.title,
-    description: params.description,
-    date: params.date, // go go calendar
-    dateCreated: new Date(),
-    maxAttendee: params.maxAttendee,
-    noAttendee: 0,
-    attendeeList: [],
-    cost: params.cost,
-    img: params.img,
-    noReported: 0,
-    adminDeleted: false,
-    tags: params.tags,
-    rating: [],
-    comment: [],
-  };
-
-  firestore
-    .collection("events")
-    .add({
-      event,
-    })
-    .then(() => {
-      alert("You have created a new event!");
-      //TODO: Redirect to eventpage
-    })
-    .catch((e) => {
-      alert("Error", e);
-    });
-
-  // }
+  // const baseEvent = GetEvent(eventId);
+  // const event = {
+  //   uid: params.uid,
+  //   title: params.title,
+  //   description: params.description,
+  //   date: params.date, // go go calendar
+  //   dateCreated: new Date(),
+  //   maxAttendee: params.maxAttendee,
+  //   noAttendee: 0,
+  //   attendeeList: [],
+  //   cost: params.cost,
+  //   img: params.img,
+  //   noReported: 0,
+  //   adminDeleted: false,
+  //   tags: params.tags,
+  //   rating: [],
+  //   comment: [],
+  // };
 };
 
+//------------- Get Event Info ----------- //
 export const GetEvent = (eventId) => {
   const [event, setEvent] = useState();
   useEffect(() => {
@@ -92,4 +80,24 @@ export const GetEvent = (eventId) => {
       });
   }, event);
   return event;
+};
+
+//------------- User Add Event ----------- //
+export const AddEventToUser = (eventId, userId) => {
+  //user reference
+  const userRef = firestore.collection("users").doc(userId);
+  //update
+  userRef.update({
+    eventHistory: firebase.firestore.FieldValue.arrayUnion(eventId),
+  });
+};
+
+//------------- Event Add User ----------- //
+export const AddUserToEvent = (eventId, userId) => {
+  //event reference
+  const eventRef = firestore.collection("events").doc(eventId);
+  //update with dot notation
+  eventRef.update({
+    "event.attendeeList": firebase.firestore.FieldValue.arrayUnion(userId),
+  });
 };
