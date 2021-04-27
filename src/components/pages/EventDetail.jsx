af6cc2c712d2fa961ceaa02e2f1cf062174fa7a9
Rittikorn Chouchonphant <61010910@kmitl.ac.th>
2021-04-28 04:31:49 +0700

    [Hotfix] Event Detail v1.1

--- src/components/pages/EventDetail.jsx
+++ src/components/pages/EventDetail.jsx
@@ -1,86 +1,92 @@
-import { useState, useEffect } from "react";
-import firestore, { auth } from "../../database/firebase";
-import "./css/EventDetail.css";
-import "../../App.css";
+import { useState, useEffect } from "react"
+import firestore, { auth } from "../../database/firebase"
+import "./css/EventDetail.css"
+import "../../App.css"
+import { useHistory } from "react-router-dom"
 
 const EventDetails = (params) => {
-  const [event, setEvent] = useState({});
-  const [host, setHost] = useState({});
-  const [uid, setUid] = useState("");
-  const [visitor, setVisitor] = useState(null);
+  const [event, setEvent] = useState({})
+  const [host, setHost] = useState({})
+  const [uid, setUid] = useState("")
+  const [visitor, setVisitor] = useState(null)
+  const history = useHistory()
+  
+  const handleEdit = () => { history.push(`/`) }
+  const handleProfile = () => { history.push(`/u/${host.uid}`) }
 
   const OwnerButton = () => {
     if (host.uid === visitor) {
       return (
         <div>
-          <button className="join-btn">Edit Event</button>
-          <button className="report-btn">Delete Event</button>
+          <button type='button' className="join-btn"> Edit Event </button>
+          <button type='button' className="report-btn"> Delete Event </button>
         </div>
       );
     } else {
       //TODO: If joined change to quit
       return (
         <div>
-          <button className="join-btn">Join Event</button>
-          <button className="report-btn">Report event</button>
+          <button type='button' className="join-btn"> Join Event </button>
+          <button type='button' className="report-btn"> Report event </button>
         </div>
       );
     }
   };
 
   async function fetchEvent() {
-    const eid = params.match.params.eventId;
+    const eid = params.match.params.eventId
     const res = await firestore
       .collection("events")
       .doc(eid)
       .get()
       .then((snapshot) => {
-        const event = snapshot.data().event;
-        setEvent(event);
-        setUid(event.uid);
+        const event = snapshot.data().event
+        setEvent(event)
+        setUid(event.uid)
       })
-      .catch((err) => alert("ERROR: ", err));
+      .catch((err) => alert("ERROR: ", err))
   }
 
   async function fetchHost() {
-    const userId = uid;
+    const userId = uid
     const res = await firestore
       .collection("users")
       .doc(userId)
       .get()
       .then((snapshot) => {
-        const user = snapshot.data();
-        setHost(user);
+        const user = snapshot.data()
+        setHost(user)
         const authState = auth.onAuthStateChanged((user) => {
-          if (user) setVisitor(user.uid);
+          if (user) setVisitor(user.uid)
         });
       })
-      .catch((err) => alert("ERROR: ", err));
+      .catch((err) => alert("ERROR: ", err))
   }
 
   useEffect(() => {
-    fetchEvent();
-  }, []);
+    fetchEvent()
+  }, [])
 
   useEffect(() => {
-    if (uid) fetchHost();
-  });
+    if (uid) fetchHost()
+  })
 
   return (
-    <div className="App-skeleton-ground">
+    <div className="App-skeleton-ground" style={{ display: 'flex', justifyContent: 'center' }}>
       {/* Host Section */}
       <div className="host-detail">
-        <p>Host Detail</p>
-        {host.displayName}
-        <img src={host.img} />
-        <br></br>
+        <h1 style={{ marginBottom: '40px' }}> Host Detail </h1>
+        <div style={{ display: 'flex', marginBottom: '40px' }}>
+          <img onClick={handleProfile} src={host.img} style={{ marginRight: '1vw', cursor: 'pointer' }} />
+          <h4> {host.displayName} </h4>
+        </div>
         <OwnerButton />
       </div>
 
       {/* Event Section */}
       <div className="event-detail">
-        <h1>{event.title}</h1>
-        <p>{event.description}</p>
+        <h1 onClick={handleEdit} style={{ marginBottom: '40px' }}> {event.title} </h1>
+        <p> {event.description} </p>
         <p>Number of Attendee : {event.noAttendee}</p>
         <p>Total of : {event.maxAttendee}</p>
         <p>Cost : {event.cost}</p>
@@ -92,4 +98,4 @@ const EventDetails = (params) => {
     </div>
   );
 };
-export default EventDetails;
+export default EventDetails
