e8fa52da20e30c0814a35d23e89443a77b15fa08
Napat Srinamon <74853232+PuiFertilizer@users.noreply.github.com>
2021-05-01 22:13:57 +0700

    add faculty

--- src/components/pages/EditUser.jsx
+++ src/components/pages/EditUser.jsx
@@ -8,8 +8,11 @@ const EditUser=(params)=>{
   const [users, setUsers] = useState({});
   const [visitor, setVisitor] = useState(null);
 
-  function handleSave(data) {
+  function handleSave() {
     //help here
+  }
+  function handlePic() {
+    
   }
   async function FetchUser() {
     const userId = params.match.params.userId;
@@ -33,45 +36,58 @@ const EditUser=(params)=>{
   return(
     <div className="App-skeleton-ground">
       <div className="App-skeleton-bg">
-        <div className="row">
-          <div className="col-2">
-            Name:
+        <form>
+          <div className="row">
+            <div className="col-2">
+              Name:
+            </div>
+            <div className="col-6">
+              {users.displayName}
+            </div>
           </div>
-          <div className="col-6">
-            {users.displayName}
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-2">
-            Pic:
-          </div>
-          <div className="col-6">
-            <img
+          <div className="row">
+            <div className="col-2">
+              Pic:
+            </div>
+            <div className="col-6">
+              <img
                 src={users.img}
                 alt={users.img}
                 height="100px"
                 width="100px"
               />
+              <br></br>
+              <button onClick={handlePic}>change picture</button>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-2">
-            Faculty:
+          <div className="row">
+            <div className="col-2">
+              Faculty:
+            </div>
+            <div className="col-6">
+              <div className="form-group">
+                <select defaultValue={"-"} name="Faculty" id="Faculty" className="form-control">
+                  <option value={"-"}>-</option>
+                  <option value={"Engineering"}>Engineering</option>
+                  <option value={"Art"}>Art</option>
+                  <option value={"Law"}>Law</option>
+                  <option value={"Agriculture"}>Agriculture</option>
+                  <option value={"Architecture"}>Architecture</option>
+                </select>
+              </div>
+            </div>
           </div>
-          <div className="col-6">
-            ...
+          <div className="row">
+            <div className="col-2">
+              Interest(s):
+            </div>
+            <div className="col-6">
+              {users.interests}
+              <button>+</button>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-2">
-            Interest(s):
-          </div>
-          <div className="col-6">
-            {users.interests}
-            <button>+</button>
-          </div>
-        </div>
-        <button onClick={handleSave}>Save</button>
+          <button onClick={handleSave}>Save</button>
+        </form>
       </div> 
     </div>
   );
