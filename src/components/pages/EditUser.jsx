5467a570e481a71267968a119a7df820c448f1ad
Napat Srinamon <74853232+PuiFertilizer@users.noreply.github.com>
2021-05-03 15:25:07 +0700

    clean up

--- src/components/pages/EditUser.jsx
+++ src/components/pages/EditUser.jsx
@@ -1,35 +1,35 @@
 import firestore, { auth } from "../../database/firebase";
 import React, { useState, useEffect } from 'react'
-import { useParams } from "react-router";
-import { Link, useHistory,} from "react-router-dom"
+import { useHistory,} from "react-router-dom"
 import '../../App.css'
 import "./css/UserDetails.css";
 import "./css/EditUser.css";
+/*import { validateFileExtension } from "../models/events";
 import validate from "../formValidate";
-import useForm from "../useForm";
+import useForm from "../useForm";*/
 
-const EditUser=({submitForm})=>{
+const EditUser=(/*{submitForm}*/)=>{
   const [users, setUsers] = useState({});
-  const [visitor, setVisitor] = useState(null);
   const history = useHistory();
-  const { handleChange, handleSubmit, input, errors } = useForm(
+  /*const { handleChange, handleSubmit, input, errors } = useForm(
     submitForm,
     validate,
-  ); 
-  function handleClick() {
-    history.push("/");
+  ); */
+
+  const [tags, setTags] = useState(["Event"]);
+  const [img, setImg] = useState(null);
+
+  function handleCancel() {
+    history.push(`/u/${users.uid}`);
   }
-  /*async function fetchUser() {
-    const userId = params.match.params.userId;
+  async function fetchUser() {
+    const userId = auth.currentUser.uid;
     const res = await firestore
       .collection("users")
       .doc(userId)
       .get()
       .then((snapshot) => {
         setUsers(snapshot.data());
-        const authState = auth.onAuthStateChanged((user) => {
-          if (user) setVisitor(user.uid);
-        });
       })
       .catch((err) => {
         alert("ERROR: ", err.message);
@@ -38,18 +38,49 @@ const EditUser=({submitForm})=>{
 
   useEffect(() => {
     fetchUser();
-  }, []);*/
+    setTags(users.interests)
+    setImg(users.img);
+  }, []);
 
+  /*useEffect(()=>{//Everytime tags change
+    //console.log(tags)
+    handleChange({
+      target:{
+        value: users.tags,
+        name: "tags"
+      }
+    })
+  },[users.tags]);*/
+
+  /*useEffect(()=>{//Everytime image change
+    //console.log("new image");
+    handleChange({
+      target:{
+        value: users.img,
+        name: "img"
+      }
+    })
+  },[users.img]);*/
+
+  /*const onImageChange = event => {
+    if (event.target.files && event.target.files[0]) {
+      const file =  event.target.files[0];
+      if(validateFileExtension(file.name)){
+        setImg(file);
+        //console.log(file);
+      }        
+    }
+  }*/
   return(
     <div className="App-skeleton-ground">
       <div style={{ marginLeft: "auto", marginRight: "auto", width: "60vw" }}>
         <h1 style={{ color: "white", fontFamily: "PT Sans", fontSize: "50px" }}>
           {" "}
-          Create Event{" "}
+          Edit User{" "}
         </h1>
       </div>
       <div className="App-skeleton-bg">
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="row">
             <div className="col-3">
               Name:
@@ -61,7 +92,7 @@ const EditUser=({submitForm})=>{
                 name="name" 
                 placeholder="Enter display name"
                 maxLength="100"
-                onChange={handleChange}
+                //onChange={handleChange}
                 defaultValue={users.displayName}
                 required
               />
@@ -72,14 +103,15 @@ const EditUser=({submitForm})=>{
               Profile Picture:
             </div>
             <div className="col-6" style={{ marginTop: "10px"}}>
-              <img
+            <img
                 src={users.img}
                 alt={users.img}
                 height="100px"
                 width="100px"
-              />
-              <br></br>
-              <button className="btn btn-outline-light" style={{ marginTop: "10px"}}>change picture</button>
+            />
+            <br></br>
+            <button className="btn btn-outline-light" style={{ marginTop: "10px"}} onClick={handleCancel}>change picture</button>
+            <br></br>
             </div>
           </div>
           <div className="row">
@@ -88,7 +120,7 @@ const EditUser=({submitForm})=>{
             </div>
             <div className="col-6" style={{ marginTop: "10px"}}>
               <div className="form-group">
-                <select defaultValue={users.faculty} name="Faculty" id="Faculty" className="form-control" onChange={handleChange}>
+                <select defaultValue={users.faculty} name="Faculty" id="Faculty" className="form-control">
                   <option value={""}></option>
                   <option value={"Engineering"}>Engineering</option>
                   <option value={"Art"}>Art</option>
@@ -108,7 +140,8 @@ const EditUser=({submitForm})=>{
               <button className="btn btn-outline-warning">+</button>
             </div>
           </div>
-          <button className="btn btn-outline-success" onClick={handleSave} style={{ marginTop: "10px"}}>Save</button>
+          <button className="btn btn-outline-success" onClick={handleCancel} style={{ marginTop: "10px"}}>Save</button>
+          <button className="btn btn-outline-danger" onClick={handleCancel} style={{ marginTop: "10px"}}>Cancel</button>
         </form>
       </div>  
       <br/>
